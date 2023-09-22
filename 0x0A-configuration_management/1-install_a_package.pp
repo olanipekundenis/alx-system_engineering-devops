d80b25182d932e1b9a1a12c470c6ce1430e04981
denis <denisstillwaters@gmail.com>
2023-09-22 11:20:28 -0700

    0x0A Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/pup
-# install a specific version of flask (2.1.0)
-
-package {'flask':
-   ensure   => '2.1.0',
-   provider => 'pip3'
+# Install puppet-lint
+package {'puppet-lint':
+    ensure   => '2.5.0',
+    provider => 'gem'
 }
