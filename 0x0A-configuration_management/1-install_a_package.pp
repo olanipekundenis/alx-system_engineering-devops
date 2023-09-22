16871387bac0d548ef3ef3e20fb0caa6838e7fe2
denis <denisstillwaters@gmail.com>
2023-09-22 10:31:17 -0700

    0x0A configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# installs puppet-lint -v 2.5.0 by executing command
+# install puppet-lint -v 2.5.0 by executing command
 
 exec { 'puppet-lint':
   command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
