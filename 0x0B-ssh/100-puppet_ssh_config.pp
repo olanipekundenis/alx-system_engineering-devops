5546e06f8ffe0c63bebb0406be43868d32abfeab
denis <denisstillwaters@gmail.com>
2023-09-26 02:04:58 -0700

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
-# using puppet to execute changes on config
-
-file_line { 'Passwd auth off':
+# Using puppet to create ssh config file
+file_line { 'Turn off passwd auth':
    ensure => 'present',
-   path => 'etc/ssh/ssh_config',
+   path => '/etc/ssh/ssh_config',
    line => '	PasswordAuthentication no',
 }
 
-file_line { 'Declare identity file':
-   ensure => 'present',
-   path => '/etc/ssh/ssh_config',
+file_line {'Declare identity file':
+   ensure => 'present'
+   path => '/etc/sshssh_config',
    line => '	IdentityFile ~/.ssh/school',
 }
-	
