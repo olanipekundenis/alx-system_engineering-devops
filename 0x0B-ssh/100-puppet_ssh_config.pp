b908a0aad51cc6850be35f8ab34d0af664fb61d1
denis <denisstillwaters@gmail.com>
2023-09-26 01:41:02 -0700

    adding 100-puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# using puppet to execute changes on config
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+	content =>
+		"
+		SSH client configuration
+		host"
+		IdentityFile ~/.ssh/school
+		PasswordAuthentication no
+}
+
